ed73789d2dfd390df3c3be8c5bbdf98b42c41659
Sheena Artrip <sheena.artrip@gmail.com>
2016-02-05 23:49:57 -0500

    remove processing loop. Do per-connection sending

--- client.go
+++ client.go
@@ -1,47 +1,18 @@
 package radius
 
 import (
+	"errors"
 	"net"
-	"sync"
-
-	"golang.org/x/net/context"
 )
 
 // A Client is the representation of a connection to a RADIUS server
 type Client struct {
-	opts     *Options
-	conn     net.Conn
-	outgoing chan *Packet
-
-	ctx    context.Context
-	cancel context.CancelFunc
-
-	lock    sync.Mutex
-	packets map[Identifier]chan *Packet
-}
-
-// Connect attemps to connect to the RADIUS server
-func (cl *Client) Connect() error {
-	conn, err := net.Dial("udp", cl.opts.Host)
-	if err != nil {
-		return err
-	}
-
-	cl.conn = conn
-
-	go cl.incomingProcessor()
-	go cl.outgoingProcessor()
-
-	return nil
+	opts *Options
 }
 
 // Close closes the client
 func (cl *Client) Close() {
-	if cl == nil {
-		return
-	}
-
-	cl.cancel()
+	// no-op
 }
 
 // Send sends a request packet to the RADIUS server
@@ -49,64 +20,28 @@ func (cl *Client) Send(p *Packet) (*Packet, error) {
 	// Inject processing objects
 	p.auth = AccountingRequestAuthenticator(cl.opts.SharedSecret)
 
-	cl.lock.Lock()
-	cl.packets[p.ID] = make(chan *Packet, 1)
-	cl.lock.Unlock()
-
-	// Send to outgoing queue
-	cl.outgoing <- p
-
-	// wait for response
-	cl.lock.Lock()
-	x := cl.packets[p.ID]
-	cl.lock.Unlock()
-	px := <-x
-
-	// clear channel from packet map
-	cl.lock.Lock()
-	delete(cl.packets, p.ID)
-	cl.lock.Unlock()
-
-	return px, nil
-}
-
-func (cl *Client) incomingProcessor() {
-	for {
-
-		select {
-		case <-cl.ctx.Done():
-			return
-		default:
-		}
-
-		var p Packet
-		if err := p.Read(cl.conn); err != nil {
-			//TODO: log error
-			continue
-		}
-
-		cl.lock.Lock()
-		c, ok := cl.packets[p.ID]
-		if !ok {
-			// TODO: log mismatching packet
-		} else {
-			c <- &p
-		}
-		cl.lock.Unlock()
+	// Make connection
+	conn, err := net.Dial("udp", cl.opts.Host)
+	if err != nil {
+		return nil, err
 	}
-}
 
-func (cl *Client) outgoingProcessor() {
-	defer cl.conn.Close()
-
-	for {
-		select {
-		case p := <-cl.outgoing:
-			if err := p.Write(cl.conn); err != nil {
-				//TODO: log error
-			}
-		case <-cl.ctx.Done():
-			return
-		}
+	// Write request
+	if err := p.Write(conn); err != nil {
+		return nil, err
 	}
+
+	// Wait for response
+	var resp Packet
+
+	if err := resp.Read(conn); err != nil {
+		return nil, err
+	}
+
+	// verify request and response have matching IDs
+	if resp.ID != p.ID {
+		return nil, errors.New("Request/Response packets do not have matching IDs")
+	}
+
+	return &resp, nil
 }
--- options.go
+++ options.go
@@ -1,29 +1,18 @@
 package radius
 
-import "golang.org/x/net/context"
-
 // Options defines the options used to connect to a RADIUS server
 // (currently only supporting accounting)
 type Options struct {
 	Host         string
 	SharedSecret string
-
-	ChannelBuffer int64
 }
 
 // NewClient creates a new client from the given options
 func (o *Options) NewClient() (*Client, error) {
 
-	ctx, cancel := context.WithCancel(context.Background())
-
 	cl := &Client{
-		opts:     o,
-		outgoing: make(chan *Packet, o.ChannelBuffer),
-		ctx:      ctx,
-		cancel:   cancel,
-		packets:  make(map[Identifier]chan *Packet),
+		opts: o,
 	}
 
-	err := cl.Connect()
-	return cl, err
+	return cl, nil
 }
